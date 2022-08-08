c7ae8dbbb86590097aed6ce9c2430be99d075257
bowlmeat <bowlmeat@qq.com>
2022-08-09 03:33:16 +0800

    first commit

---
+++ src/main/java/com/itheima/reggie/ReggieMasterApplication.java
@@ -0,0 +1,18 @@
+package com.itheima.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Slf4j
+@SpringBootApplication
+@ServletComponentScan // 自动扫描webFilter
+@EnableTransactionManagement // 开启事务
+public class ReggieMasterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieMasterApplication.class, args);
+        log.info("项目加载成功");
+    }
+}
---
+++ src/test/java/com/example/reggiemaster/ReggieMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reggiemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReggieMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
